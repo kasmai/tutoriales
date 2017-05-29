b2eb52560b0aa60dc510ef55a15271cfe9fad35e
Susana <susana.gonzalez@mrm-mccann.com>
2017-05-29 18:01:58 -0500

    add login v1.2

--- login.php
+++ login.php
@@ -13,11 +13,27 @@ include 'header.php';
 	<link href="hm/css/hover.css" rel="stylesheet" media="all">
 	<link rel="icon" type="image/png" href="favicon (1).ico">
 </head>
-<body class="mn">
+<body class="log">
 		
 		<div class="container mlogin">
 			<div class="item thumb2" data-width="282">	
 			<center><h2>Log in</h2></center>	
+			<form name="loginform" id="loginform" action="" method="POST">
+			 <p>
+			 <label for="user_login">Nombre De Usuario<br />
+			 <input type="text" name="username" id="username" class="input" value="" size="20" /></label>
+			 </p>
+			 <p>
+			 <label for="user_pass">Contraseña<br />
+			 <input type="password" name="password" id="password" class="input" value="" size="20" /></label>
+			 </p>
+			 <p class="submit">
+			 <input type="submit" name="login" class="button" value="Entrar" />
+			 </p>
+			 <p class="regtext">No estas registrado? <a href="register.php" >Registrate Aquí</a>!</p>
+			</form>
+
+
 			</div>
 		</div>
 									
